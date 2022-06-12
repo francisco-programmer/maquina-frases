0b2f6ffad94848309721d3b2c27c9691ab2a17e1
francisco-programmer <franciscopalenciaa@gmai.com>
2022-06-11 22:13:37 -0400

    se agrega

---
+++ src/componentes/Quote.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Quote = ({ quote }) => {
+  return (
+    <div> 
+       <p>{ quote.text }<br/>
+    <span>{ quote.author}</span>
+       </p>
+    </div>
+  )
+}
+
+export default Quote
